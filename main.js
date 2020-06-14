76647950d6c6cd2a82bc9c8fa7123a5be28de19e
gau4x4 <50134400+gau4x4@users.noreply.github.com>
2020-06-14 17:28:06 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 let calculateShortestDistance=(argArray)=>{
   let tempSmVal=Number.MAX_SAFE_INTEGER,arrV=[];
-  for(let i=0;i<argArray.length-1;i++){
-    if(tempSmVal>Math.abs(argArray[i],argArray[i+1])){
-      tempSmVal=Math.abs(argArray[i]-argArray[i+1]);
-     
+  for(let j=0;j<argArray.length;j++){
+    for(let i=j+1;i<argArray.length;i++){
+      if(tempSmVal>Math.abs(argArray[j],argArray[i])){
+        tempSmVal=Math.abs(argArray[j]-argArray[i]);
+      }
+      arrV.push(tempSmVal);
     }
-    arrV.push(tempSmVal);
   }
- arrV.sort((a,b)=> a-b);
+  arrV.sort((a,b)=> a-b);
+console.log(arrV)
   return arrV.shift();
 }
-const testArray=[1,20,24,19,28,13,5,1,2];
+const testArray=[17,1,24,19,28,13,57,2];
 console.log(calculateShortestDistance(testArray));
